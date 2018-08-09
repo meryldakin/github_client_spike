10dc508de5e60a721bbf8d3c5d49c0134885e31b
Alex Griffith <alex.griffith@flatironschool.com>
2018-08-09 17:42:30 -0400

    Geeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef

--- lib/github_client_spike.ex
+++ lib/github_client_spike.ex
@@ -1,4 +1,17 @@
 defmodule GithubClientSpike do
+  # alias Geef.Repository
+  # alias Geef.Reference
+  # alias Geef.Commit
+  # alias Geef.Tree
+  # alias Geef.Oid
+  # alias Geef.Object
+  # alias Geef.Signature
+  # sig = Signature.now("alexgriff", "alexalexgriffith@gmail.com")
+  # repo = Repository.open!("./temp/2018-08-09-19\:15\:15.696576Z")
+  # {_, commit} = Geef.Commit.lookup(repo, Oid.parse("3a0d892c7ad919f269d15a80477241e19342c951"))
+  # tree_id = Commit.tree_id(commit)
+  # {_, tree} = Tree.lookup(repo, tree_id)
+
   @access_token System.get_env("GHE_TOKEN")
   @url "https://gh.learn.co/api/v3/"
   @client Tentacat.Client.new(%{access_token: @access_token}, @url)
@@ -20,12 +33,12 @@ defmodule GithubClientSpike do
     {_status, teams, _resp} = Tentacat.Organizations.Teams.list(@client, org_name)
 
     Enum.find(teams, fn team -> team["name"] == team_name end)
-    |> get_team_id(%{org_name: org_name, team_name: team_name})
+    |> team_id(%{org_name: org_name, team_name: team_name})
   end
 
-  def get_team_id(%{"id" => id}, %{org_name: _, team_name: _}), do: id
+  def team_id(%{"id" => id}, %{org_name: _, team_name: _}), do: id
 
-  def get_team_id(_, %{org_name: org_name, team_name: team_name}) do
+  def team_id(_, %{org_name: org_name, team_name: team_name}) do
     params = %{"name" => team_name, "private" => "closed"}
     {_status, %{"id" => id}, _resp } = Tentacat.Organizations.Teams.create(@client, org_name, params)
     id
@@ -41,7 +54,12 @@ defmodule GithubClientSpike do
   end
 
   def clone do
-    System.cmd("git", ["clone", "git@github.com:alexgriff/hidden_phrase_frontend.git", "temp/#{DateTime.utc_now}"])
+    now = DateTime.utc_now
+    |> DateTime.to_string
+    |> String.split(" ")
+    |> Enum.join("-")
+
+    System.cmd("git", ["clone", "git@github.com:alexgriff/hidden_phrase_frontend.git", "temp/#{now}"])
   end
 
 
